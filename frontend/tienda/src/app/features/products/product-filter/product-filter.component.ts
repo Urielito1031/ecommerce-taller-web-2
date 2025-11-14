9c847cb63b8767042169f098a9ea239cf55dfbed
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2025-11-14 10:46:14 -0300

    filtro

--- frontend/tienda/src/app/features/products/product-filter/product-filter.component.ts
+++ frontend/tienda/src/app/features/products/product-filter/product-filter.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.css'
 })
+
 export class ProductFilterComponent {
 
 }
